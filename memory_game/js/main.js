cd2a7634c3adfb7401eace1d518733dbf70e06fa
YahyaMo <ya7yamo1@gmail.com>
2018-11-02 18:26:40 +0300

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "queen";
+var cardtwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User flipped " + cardOne); // queen
+console.log("User flipped " + cardThree); //king
