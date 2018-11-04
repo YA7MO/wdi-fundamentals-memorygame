3c6efe883946a8668dd386d5bebc98a11b99ac64
YahyaMo <ya7yamo1@gmail.com>
2018-11-04 20:09:19 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,39 @@
 console.log("Up and running!");
 
+/*
+created in the previous project
 var cardOne = "queen";
-var cardtwo = "queen";
+var cardTwo = "queen";
 var cardThree = "king";
-var cardFour = "king";
-
+var cardFour = "king"; 
 console.log("User flipped " + cardOne); // queen
 console.log("User flipped " + cardThree); //king
+
+*/
+
+var cards = ["queen" ,"queen", "king", "king"]; // array holds the type of cards.
+var cardsInPlay = [];
+var cardOne = cards[0]; // first card 
+cardsInPlay.push(cardOne);
+
+console.log("player flipped queen");
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("palyer flipped king");
+
+//console.log(cardsInPlay); testing 
+
+if(cardsInPlay.length === 2){
+	
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}
+	else{
+		alert("Sorry, try again");
+	}
+
+}
+	 
+	
+		
