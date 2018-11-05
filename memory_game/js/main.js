4336f7f03c07e93637d1f1d47b6960c77f7c5937
YahyaMo <ya7yamo1@gmail.com>
2018-11-05 20:19:39 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -11,7 +11,7 @@ console.log("User flipped " + cardThree); //king
 
 */
 // object-array
-
+var reset_board = document.getElementById('game-board-reset-button');
 var cards = [
 {
 rank: "queen",
@@ -45,37 +45,55 @@ var checkForMatch = function()
 
 	if (cardsInPlay[0] === cardsInPlay[1]) 
 	{
-console.log("You found a match!");
-} else {
-console.log("Sorry, try again.");
-}
-}
-
-var flipCard = function(cardId)
-{
-//console.log(cardsInPlay); testing 
-console.log("user flipped " + cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
-	cardsInPlay.push(cards[cardId].rank);
-
-if(cardsInPlay.length === 2)
-{
-	
-	if(cardsInPlay[0] === cardsInPlay[1])
-	{
+		console.log("You found a match!");
 		alert("You found a match!");
-	}
-	else{
+
+	} else {
+		console.log("Sorry, try again.");
 		alert("Sorry, try again");
 	}
 
-	checkForMatch();
+	reset_board.innerHTML = '<button id="reset_board">Start Over</button>';
 
 }
-}
- flipCard(0);
- flipCard(2);
 
+function resetBoardButton() {
+	location.reload();
+}
+
+var flipCard = function()
+{
+	//console.log(cardsInPlay); testing 
+	var cardId = this.getAttribute('data-id');
+	console.log("user flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
+	var newImage = cards[cardId].cardImage;
+	this.setAttribute('src',newImage);
+
+	if(cardsInPlay.length === 2)
+	{
+		checkForMatch();
+
+	}
+}
+
+function createBoard() {
 	
+	for(var i =0; i < cards.length; i++){
+		 var cardElement = document.createElement('img');
+		 cardElement.setAttribute('src', "images/back.png");
+		 cardElement.setAttribute('class', "deckImage");
+		 cardElement.setAttribute('data-id',i);
+		 cardElement.addEventListener('click',flipCard);
+		 var game_board = document.getElementById('game-board');
+		 game_board.appendChild(cardElement);
+
+	}
+}
+
+createBoard();
+
+reset_board.addEventListener('click',resetBoardButton);	
 		
